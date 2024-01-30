c72e8017636002d62896f85f5f029becf3be41eb
LuisMauro <152839866+LuisMauro@users.noreply.github.com>
2024-01-30 01:34:26 -0300

    Add files via upload

--- libros.js
+++ libros.js
@@ -2,44 +2,47 @@ const express = require("express");
 const router = express.Router();
 const Libro = require("../models/Libro");
 
-router.get("/", async (req, res) => {
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
+
+router.get("/", requiredScopes("read:libros"), async (req, res) => {
 try {
-const libros = await Libro.find();
-res.json(libros);
-} catch (error) {
-res.status(500).json({ error: "Error al obtener los libros" });
+  const libros = await Libro.find();
+  res.json(libros);
+  }catch (error) {
+  res.status(500).json({ error: "Error al obtener los libros" });
 }
 });
 
-router.post("/", async (req, res) => {
-try {
-const nuevoLibro = new Libro(req.body);
-await nuevoLibro.save();
-res.json(nuevoLibro);
-} catch (error) {
-res.status(500).json({ error: "Error al crear el Libro" });
-}
-});
 
-router.put("/:id", async (req, res) => {
+router.post("/", requiredScopes("write:libros"), async (req, res) => {
   try {
-     const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
-     {
-      new: true,
-     });
-     res.json(Libro);
-    } catch (error) {
-     res.status(500).json({ error: "Error al actualizar el Libro" });
-    }
+    const nuevoLibro = new Libro(req.body);
+    await nuevoLibro.save();
+    res.json(nuevoLibro);
+    }catch (error) {
+     res.status(500).json({ error: "Error al crear el Libro" });
+  }
+ });
+
+router.put("/:id", requiredScopes("write:libros"), async (req, res) => {
+  try {
+   const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
+   {
+   new: true,
+ });
+   res.json(Libro);
+  }catch (error) {
+   res.status(500).json({ error: "Error al actualizar el Libro" });
+ }
 });
-       
-router.delete('/:id', async (req, res) => {
+ 
+router.delete("/:id", requiredScopes("write:libros"), async (req, res) => {
   try {
-      await Libro.findByIdAndDelete(req.params.id);
-      res.json({ message: 'Libro eliminado correctamente' });
-      } catch (error) {
-      res.status(500).json({ error: 'Error al eliminar el Libro' });
-      }
+    await Libro.findByIdAndDelete(req.params.id);
+    res.json({ message: "Libro eliminado correctamente" });
+   }catch (error) {
+    res.status(500).json({ error: "Error al eliminar el Libro" });
+   }
 });
 module.exports = router;
         
